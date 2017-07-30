27e2e79cf5826e12f94f1fe26ba2586afb1dcb08
iluccifer <viktor.kucherenko.v@gmail.com>
2017-07-30 03:37:42 +0300

    lessons

---
+++ src/test/java/Selenium/DragAndDrop.java
@@ -0,0 +1,31 @@
+package advancedinteractions.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import selenium.core.WebDriverTestBase;
+
+public class DragAndDrop extends WebDriverTestBase {
+    @Test
+    public void testDragDropTest1() throws InterruptedException {
+        webDriver.get("https://gojs.net/latest/samples/htmlDragDrop.html?gclid=CLjWnLeG5tQCFUeVGwodCowEcA");
+        WebElement from = webDriver.findElement(By.xpath(".//*[@id='sample']/div[1]/span[1]/div/div[1]"));
+        WebElement to = webDriver.findElement(By.xpath(".//*[@id='myDiagramDiv']/canvas"));
+
+        Actions builder = new Actions(webDriver);
+        builder.clickAndHold(from).moveToElement(to).release(to).build().perform();
+        from.getAttribute("src").equals("img_logo.gif");
+    }
+    @Test
+    public void testDragDropTest2() throws InterruptedException {
+        webDriver.get("https://gojs.net/latest/samples/htmlDragDrop.html?gclid=CLjWnLeG5tQCFUeVGwodCowEcA");
+        WebElement from = webDriver.findElement(By.xpath(".//*[@id='sample']/div[1]/span[1]/div/div[1]"));
+        WebElement to = webDriver.findElement(By.xpath(".//*[@id='myDiagramDiv']/canvas"));
+
+        Actions builder = new Actions(webDriver);
+        builder.dragAndDrop(from, to).build().perform();
+        from.getAttribute("src").equals("img_logo.gif");
+        webDriver.quit();
+    }
+}
